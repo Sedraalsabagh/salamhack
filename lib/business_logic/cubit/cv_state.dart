0da537f9d08b698e48020ba927ac5d82a564fc4f
Sedraalsabagh <sedraalsabbagh2021@gmail.com>
2025-03-14 20:15:37 +0300

    fix bug

--- lib/business_logic/cubit/cv_state.dart
+++ lib/business_logic/cubit/cv_state.dart
@@ -1,6 +1,18 @@
 part of 'cv_cubit.dart';
 
 @immutable
-sealed class CvState {}
+abstract class CvState {}
 
-final class CvInitial extends CvState {}
+class CVInitial extends CvState {}
+
+class CVLoading extends CvState {}
+
+class CVSuccess extends CvState {
+  final dynamic response;
+  CVSuccess(this.response);
+}
+
+class CVFailure extends CvState {
+  final String error;
+  CVFailure(this.error);
+}
