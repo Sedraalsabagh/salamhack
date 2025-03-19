74c4c4a8d67f88c9a17f5d901588db39f91ee1c3
sara00abbas <sara.abbas.ab@gmail.com>
2025-03-19 12:36:25 +0300

    edit evaluate

---
+++ lib/business_logic/cubit/evaluations_cubit.dart
@@ -0,0 +1,29 @@
+import 'package:bloc/bloc.dart';
+import 'package:devloper_app/business_logic/cubit/evaluations_state.dart';
+import 'package:devloper_app/data/repository/evaluation.dart';
+
+class ResumeCubit extends Cubit<ResumeState> {
+  final ResumeRepository repository;
+
+  ResumeCubit(this.repository) : super(ResumeInitial());
+
+  Future<void> evaluateResume({
+    required String jobDescription,
+    String? filePath,
+    List<int>? fileBytes,
+    String? fileName,
+  }) async {
+    emit(ResumeLoading());
+    try {
+      final result = await repository.evaluateResume(
+        jobDescription: jobDescription,
+        filePath: filePath,
+        fileBytes: fileBytes,
+        fileName: fileName,
+      );
+      emit(ResumeLoaded(result));
+    } catch (e) {
+      emit(ResumeError(e.toString()));
+    }
+  }
+}
---
+++ lib/business_logic/cubit/evaluations_state.dart
@@ -0,0 +1,27 @@
+import 'package:equatable/equatable.dart';
+import 'package:devloper_app/data/models/evaluation.dart';
+
+abstract class ResumeState extends Equatable {
+  @override
+  List<Object> get props => [];
+}
+
+class ResumeInitial extends ResumeState {}
+
+class ResumeLoading extends ResumeState {}
+
+class ResumeLoaded extends ResumeState {
+  final ResumeEvaluationResult result;
+  ResumeLoaded(this.result);
+
+  @override
+  List<Object> get props => [result];
+}
+
+class ResumeError extends ResumeState {
+  final String message;
+  ResumeError(this.message);
+
+  @override
+  List<Object> get props => [message];
+}
